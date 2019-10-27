45f33414e60eb1e00305ba3a75d58d450e916a8c
marcelowukong <56727193+marcelowukong@users.noreply.github.com>
2019-10-27 00:35:23 -0300

    Regression_Model

---
+++ packages/regression_model/regression_model/config/config.py
@@ -0,0 +1,22 @@
+import pathlib
+
+PACKAGE_ROOT = pathlib.Path(__file__).resolve().parents[1]
+TRAINED_MODEL_DIR = PACKAGE_ROOT / 'trained_models'
+DATASET_DIR = PACKAGE_ROOT / 'datasets'
+
+# data
+TESTING_DATA_FILE = 'test.csv'
+TRAINING_DATA_FILE = 'train.csv'
+TARGET = 'SalePrice'
+
+
+# variables
+FEATURES = ['MSSubClass', 'MSZoning', 'Neighborhood',
+            'OverallQual', 'OverallCond', 'YearRemodAdd',
+            'RoofStyle', 'MasVnrType', 'BsmtQual', 'BsmtExposure',
+            'HeatingQC', 'CentralAir', '1stFlrSF', 'GrLivArea',
+            'BsmtFullBath', 'KitchenQual', 'Fireplaces', 'FireplaceQu',
+            'GarageType', 'GarageFinish', 'GarageCars', 'PavedDrive',
+            'LotFrontage',
+            # this one is only to calculate temporal variable:
+            'YrSold']
